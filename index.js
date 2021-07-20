646c89294fd17042bff7e43c0b031385069a95a9
Robin Cunningham <robin.cunningham@thinkmoney.co.uk>
2021-07-20 15:17:48 +0100

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-function sum(a, b) {
-    return a + b;
+class Account {
+    constructor() {
+        this.balance = 0;
+        this.history = [];
+    }
+
+    deposit(amount) {
+        this.balance += amount;
+    }
+
+    withdraw(amount) {
+        this.balance -= amount;
+    }
+
+    updateHistory(date, amount, balance) {
+        this.history.push({ date: date, amount: amount, balance: balance });
+    }
 }
+
 module.exports = {
-    sum,
+    Account,
 };
--- index.test.js
+++ index.test.js
@@ -1,5 +1,49 @@
-const { sum } = require('./index.js');
+const { Account } = require('./index.js');
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+// before each that init a new Account
+// add in describe blocks
+
+test('that balance returns 0', () => {
+    let balance = new Account().balance;
+    expect(balance).toBe(0);
+});
+
+test('that a amount is added to the balance', () => {
+    let account = new Account();
+    account.deposit(5);
+    expect(account.balance).toBe(5);
+});
+
+test('that a amount can be added to the balance multiple times', () => {
+    let account = new Account();
+    account.deposit(5);
+    account.deposit(5);
+    expect(account.balance).toBe(10);
+});
+
+// stretch goal is to ensure deposit can only be called with positive numbers
+
+test('that an amount can withdrawn from the balance', () => {
+    let account = new Account();
+    account.withdraw(5);
+    expect(account.balance).toBe(-5);
+});
+
+
+test('that a amount can be withdrawn from the balance multiple times', () => {
+    let account = new Account();
+    account.withdraw(5);
+    account.withdraw(5);
+    expect(account.balance).toBe(-10);
+});
+
+test('that account has a transaction history', () => {
+    let account = new Account();
+    expect(account.history).toEqual([]);
+});
+
+test('that updateHistory adds to the transaction history', () => {
+    let account = new Account();
+    account.updateHistory('14/01/2021', -500, 2500);
+    expect(account.history).toEqual([{ date: '14/01/2021', amount: -500, balance: 2500 }]);
 });
